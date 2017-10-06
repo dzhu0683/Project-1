c5c9bf5555bbf847d734e8bfe650854f4700bdc2
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-10-06 09:28:09 -0400

    1

--- Project 1/src/dzhulib.java
+++ Project 1/src/dzhulib.java
@@ -1,8 +1,11 @@
 
 public class dzhulib 
 {
-	public static void fs()
+	public static void dateStr()
 	{
+		Scanner date = new Scanner(System.in)
+		System.out.println("Enter a date in the format (mm/dd/yyyy)")
+		in a = reader.nextline();
 		
 	}
 }
