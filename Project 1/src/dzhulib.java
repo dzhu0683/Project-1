bb0be8a6d73e6e9b75d6bfa411320067876bfad7
Darren <Darren@Darren-PC>
2017-10-09 12:01:57 -0400

    5

--- Project 1/src/dzhulib.java
+++ Project 1/src/dzhulib.java
@@ -1,9 +1,11 @@
-
+import java.util.Scanner;
 public class dzhulib 
 {
 	public static void dateStr()
 	{
-		
+		//Scanner integer = new Scanner(System.in);
+		//System.out.print("Input date in the format mm/dd/yyyy.");
+		//String old = 
 	}
 	public static void cutOut()
 	{
